aae346e3f0a2f94da0ca18661b3170717ebafe2d
yzhao0527 <yzhao0527@gmail.com>
2017-06-26 21:41:57 -0400

    use try catch block to improve robustness

--- proc/exec_snapshot_crypto_crncy_market_data.py
+++ proc/exec_snapshot_crypto_crncy_market_data.py
@@ -16,17 +16,26 @@ from qfin.CryptoCrncy.proc import (
         snapshot_MarketDataL2_GDAX, 
     )
 
+import logging  # @ToDo: use logging instead of print
+
 
 def run():
 
+    wait = 1
     while True:
         sleep(1)
 
         now = utcnow()
         print('running %s' % now.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]) 
 
-        cnt = snapshot_MarketDataL2_GDAX()
-        print("GDAX: loaded %s rows" % cnt)
+        try:
+            cnt  = snapshot_MarketDataL2_GDAX()
+            print("GDAX: loaded %s rows" % cnt)
+            wait = 1
+        except Exception as err:
+            print("GDAX: failed to load market data")
+            sleep(wait)
+            wait *= 2
 
 
 if __name__ == '__main__':
